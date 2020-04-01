b8c05ac4124cf6176656276d6f59ff6e5c8c8507
iamkarlotolentino <tolentinojancarlo@gmail.com>
2020-04-02 02:17:10 +0800

    Update project.asm

--- project.asm
+++ project.asm
@@ -1,6 +1,5 @@
 
 title "Bank Account Manager"
-
 .MODEL small
 .STACK 100h
 .DATA
@@ -9,6 +8,11 @@ title "Bank Account Manager"
     
     balance_amnt    db  13, ?, 13 dup(' ')
     input_amount    db  13, ?, 13 dup(" ")
+    
+    fname           db  'account.txt', 0
+    fhandle         dw  ?
+    fbuffer         db  25 dup(?)
+    fcount          db  10
     
     time            db  "Time:$"
     timestamp       db  "00:00:00$"
@@ -32,40 +36,38 @@ title "Bank Account Manager"
 	
     exit_msg        db "Thank you. Goodbye!$"
     app_blank       db "                                    $"
-    app_version     db "Teller Machine @ 2020 ", 179d, " v1.0$" 	
-.CODE
+    app_version     db "Teller Machine @ 2020 ", 179d, " v1.0$"
+    
 ;**************************************************************************;
 ;------------------------------   MACRO   --------------------------------;
 ;**************************************************************************;
 cursor_at MACRO row,column
-    pusha
-    mov dh, row
-    mov dl, column
-    mov bh, 0
-    mov ah, 2
-    int 10h
-    popa
+    mov           dh, row
+    mov           dl, column
+    mov           bh, 0
+    mov           ah, 2
+    int           10h
 ENDM
 
 ; string scanner
 reads MACRO buffer
     pusha
-    lea dx, buffer
-	mov ah, 0ah
-	int 21h
-	xor bx, bx
-	mov bl, buffer[1]
-	mov buffer[bx+2], '$'
-	lea dx, buffer + 2
+    lea           dx, buffer
+	mov           ah, 0ah
+	int           21h
+	xor           bx, bx
+	mov           bl, buffer[1]
+	mov           buffer[bx+2], '$'
+	lea           dx, buffer + 2
 	popa
 ENDM
 
 ; string printing
 prints MACRO msg
     pusha
-    mov dx, offset msg
-    mov ah, 9
-    int 21h
+    mov           dx, offset msg
+    mov           ah, 9
+    int           21h
     popa
 ENDM
 
@@ -93,7 +95,8 @@ alert MACRO msg
     call cls
     prints msg
     popa
-ENDM
+ENDM 	
+.CODE
 
 main PROC
     call setup
@@ -121,19 +124,21 @@ main PROC
 main ENDP
 
 setup PROC
+    ; data segment address
+    mov dx, @data
+    mov ds, dx
+	mov es, dx
+    
     ; set video mode
 	mov al, 00h
 	mov ah, 0
 	int 10h
 	
-	;mov ax, 1003h ; disable blinking.  
-    ;mov bx, 0        
-    ;int 10h
-    
-    mov ch, 32
-    mov ah, 1
-    int 10h
-    
+	; disable blinking
+    mov ax, 1003h
+    mov bx, 0
+    int 10h   
+       
     ; hide text cursor:
     mov ch, 32
     mov ah, 1
@@ -147,188 +152,204 @@ setup PROC
     mov ax, 1
     int 33h
     
-    ; data segment address
-    mov ax, @data
-	mov ds, ax
+    ; load account details
+    mov ah, 3Dh
+    lea dx, fname
+    mov al, 0
+    int 21h
+    mov fhandle, ax
+    
+    mov ah, 3Fh
+    mov bx, fhandle
+    lea dx, fbuffer
+    mov cx, 10
+    int 21h 
 	
-	call cls
     ret   
 setup ENDP
 
 login_page PROC
-    cursor_at 3,2
-    printc _sym[5]
-	printr _sym[9],34
-	printc _sym[2]
+    l1:
+        mov ah, 2
+        mov dl, [si]
+        int 21h
+        inc si
+        dec fcount
+        jnz l1
+    
+    
+    cursor_at      3,2
+    printc         _sym[5]
+	printr         _sym[9],34
+	printc         _sym[2]
 	
-	cursor_at 4,2
-	printc _sym[1]
-	cursor_at 4,37
-	printc _sym[1]
-	cursor_at 4,4
-	prints login_accnt
+	cursor_at     4,2
+	printc        _sym[1]
+	cursor_at     4,37
+	printc        _sym[1]
+	cursor_at     4,4
+	prints        login_accnt
 	
-	cursor_at 5,2
-	printc _sym[1]
-	cursor_at 5,37
-	printc _sym[1]                                                                       
-	cursor_at 5,4
-	prints login_desc
+	cursor_at     5,2
+	printc        _sym[1]
+	cursor_at     5,37
+	printc        _sym[1]                                                                       
+	cursor_at     5,4
+	prints        login_desc
 	
-	cursor_at 6,2
-	printc _sym[4]
-	printr _sym[9],34
-	printc _sym[3]
+	cursor_at     6,2
+	printc        _sym[4]
+	printr        _sym[9],34
+	printc        _sym[3]
 	 
-	cursor_at   8,2
-	printc _sym[5]
-	printr _sym[9],12
-	printc _sym[7]
-	printr _sym[9],21
-	printc _sym[2]
+	cursor_at     8,2
+	printc        _sym[5]
+	printr        _sym[9],12
+	printc        _sym[7]
+	printr        _sym[9],21
+	printc        _sym[2]
 	
-	cursor_at 9,2
-	printc _sym[1]
-	cursor_at 9,15
-	printc _sym[1]
-	cursor_at 9,37
-	printc _sym[1]
-	cursor_at 9,4
-	prints login_frm_text[0]
+	cursor_at     9,2
+	printc        _sym[1]
+	cursor_at     9,15
+	printc        _sym[1]
+	cursor_at     9,37
+	printc        _sym[1]
+	cursor_at     9,4
+	prints        login_frm_text[0]
 	
-	cursor_at 10,2
-	printc _sym[8]
-	printr _sym[9],12
-	printc _sym[10]
-	printr _sym[9],21
-	printc _sym[0]
+	cursor_at     10,2
+	printc        _sym[8]
+	printr        _sym[9],12
+	printc        _sym[10]
+	printr        _sym[9],21
+	printc        _sym[0]
 
-	cursor_at 11,2
-	printc _sym[1]
-	cursor_at 11,4
-	prints login_frm_text[9]
-	cursor_at 11,15
-	printc _sym[1]
-	cursor_at 11,37
-	printc _sym[1]
+	cursor_at     11,2
+	printc        _sym[1]
+	cursor_at     11,4
+	prints        login_frm_text[9]
+	cursor_at     11,15
+	printc        _sym[1]
+	cursor_at     11,37
+	printc        _sym[1]
 	
-	cursor_at 12,2
-	printc _sym[4]
-	printr _sym[9],12
-	printc _sym[6]
-	printr _sym[9],21
-	printc _sym[3]
+	cursor_at     12,2
+	printc        _sym[4]
+	printr        _sym[9],12
+	printc        _sym[6]
+	printr        _sym[9],21
+	printc        _sym[3]
 	
-	cursor_at 14,6
-	prints login_frm_sel[0]
-	cursor_at 14,18
-	prints login_frm_sel[7]
+	cursor_at     14,6
+	prints        login_frm_sel[0]
+	cursor_at     14,18
+	prints        login_frm_sel[7]
 	
-	cursor_at   23,2
-	prints      app_version
+	cursor_at     23,2
+	prints        app_version
 	
 	; listens to mouse-pos
 	keep_listening:
-    	; get mouse state
-    	mov ax, 3
-    	int 33h
-    	; left button click
-    	cmp bx, 1
-    	jne keep_listening
+        ; get mouse state
+        mov           ax, 3
+        int           33h
+        ; left button click
+    	cmp           bx, 1
+    	jne           keep_listening
     	; passes when left button is pressed
     	
     	; form-selected detection
-    	cmp cx, 0080h
-    	jnge menu_selection
-    	cmp cx, 0126H
-    	jnle menu_selection
+    	cmp           cx, 0080h
+    	jnge          menu_selection
+    	cmp           cx, 0126H
+    	jnle          menu_selection
     	
     	card_no:
-        	cmp         dx, 0050h
-        	jnle        pin_code
-        	cmp         dx, 0048h
-        	jnge        menu_selection
-        	cursor_at   9,16
-        	reads       in_card_no
+            cmp           dx, 0050h
+            jnle          pin_code
+        	cmp           dx, 0048h
+        	jnge          menu_selection
+        	cursor_at     9,16
+        	reads         in_card_no
     	pin_code:
-        	cmp         dx, 005Eh
-        	jnle        menu_selection
-        	cmp         dx, 0059h
-        	jnge        menu_selection
-        	cursor_at   11,16
-    	    reads       in_pin_code
+        	cmp           dx, 005Eh
+        	jnle          menu_selection
+        	cmp           dx, 0059h
+        	jnge          menu_selection
+        	cursor_at     11,16
+    	    reads         in_pin_code
     	    
     	; ok&cancel menu detection 
     	menu_selection:
-        	cmp     dx, 006Eh
-        	jnge    keep_listening
-        	cmp     dx, 0075h
-        	jnle    keep_listening
+        	cmp           dx, 006Eh
+        	jnge          keep_listening
+        	cmp           dx, 0075h
+        	jnle          keep_listening
     	cancel_button:
-        	cmp     cx, 0090h
-        	jnge    ok_button
-        	cmp     cx, 00DFh
-        	jnle    keep_listening
-        	call sys_exit
+        	cmp           cx, 0090h
+        	jnge          ok_button
+        	cmp           cx, 00DFh
+        	jnle          keep_listening
+        	call          sys_exit
     	ok_button:
-    	    cmp     cx, 0030h
-        	jnge    keep_listening
-        	cmp     cx, 005Fh
-        	jnle    keep_listening
+    	    cmp           cx, 0030h
+        	jnge          keep_listening
+        	cmp           cx, 005Fh
+        	jnle          keep_listening
         	; TODO: Check credibility first
     ret	 
 login_page ENDP
 
 menu_page PROC
     menu_start:
-        call cls                                       
+        ;call          cls                                       
         ; al=row start
-        mov bl,   7
-        mov cl,   3
+        mov bl,       7
+        mov cl,       3
         create_block:
-            cursor_at   bl,2
-            prints      menu_hdr
-            cursor_at   bl,20
-            prints      menu_hdr
-            inc bl
-            cursor_at   bl,2
-            printc      _sym[1]
-            cursor_at   bl,18
-            printc      _sym[1]
-            cursor_at   bl,20
-            printc      _sym[1]
-            cursor_at   bl,36
-            printc      _sym[1]
-            inc bl
-            cursor_at   bl,2
-            prints      menu_frm_ftr
-            cursor_at   bl,20
-            prints      menu_frm_ftr
-            inc bl
-            inc bl
-        loop create_block
+            cursor_at     bl,2
+            prints        menu_hdr
+            cursor_at     bl,20
+            prints        menu_hdr
+            inc           bl
+            cursor_at     bl,2
+            printc        _sym[1]
+            cursor_at     bl,18
+            printc        _sym[1]
+            cursor_at     bl,20
+            printc        _sym[1]
+            cursor_at     bl,36
+            printc        _sym[1]
+            inc           bl
+            cursor_at     bl,2
+            prints        menu_frm_ftr
+            cursor_at     bl,20
+            prints        menu_frm_ftr
+            inc           bl
+            inc           bl
+        loop          create_block
         
         ; withdraw-text
-        cursor_at 8,6
-        prints menu_frm_text[0]
+        cursor_at     8,6
+        prints        menu_frm_text[0]
         ; deposit-text
-        cursor_at 8,25
-        prints menu_frm_text[9]
+        cursor_at     8,25
+        prints        menu_frm_text[9]
         ; balance-text
-        cursor_at 12,7
-        prints menu_frm_text[17]
+        cursor_at     12,7
+        prints        menu_frm_text[17]
         ; reset-pin text
-        cursor_at 12,24
-        prints menu_frm_text[25]
+        cursor_at     12,24
+        prints        menu_frm_text[25]
         ; log-out text
-        cursor_at 16,7
-        prints menu_frm_text[35]
+        cursor_at     16,7
+        prints        menu_frm_text[35]
         ; details text
-        cursor_at 16,25
-        prints menu_frm_text[43]
+        cursor_at     16,25
+        prints        menu_frm_text[43]
         
         render_loop:
-            
             ; time printing
             lea bx,timestamp           
             call get_time               
@@ -497,7 +518,7 @@ get_time PROC
     pop cx
     pop ax  
 
-    RET                          
+    ret
 get_time ENDP 
 
 to_ascii PROC
